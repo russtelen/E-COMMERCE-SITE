bc1a7772b9cc703f1c00f2cbf69e4a3935a7d23f
veeepi <vpreduna90@gmail.com>
2020-10-22 09:05:22 -0700

    fixed +/- count

--- scripts/cart.js
+++ scripts/cart.js
@@ -15,23 +15,11 @@ let items = [
 
 // Add to Cart Buttons
 // collection - Cart Buttons
-let carts = document.querySelectorAll('.add-cart') 
-for (let i = 0; i < carts.length; i++) {
-    carts[i].addEventListener('click', () => {
-        cartQuantity(items[i]);
-        totalCost(items[i]);
-        displayCart()
-    });
-} 
+
 // define var - increase qty
 // Page Refresh: update NavBar Cart itemCount from LocalStorage itemCount
-function onLoadCartQuantity(){ // CALLED at END of script
-    let itemQuantity = parseInt(localStorage.getItem('cartQuantity'));
-    if (itemQuantity) { // if Int exists, display it  
-        document.querySelector('.nav-cart-count').textContent = itemQuantity;
-    } 
-}
-function cartQuantity(item) { // increments 'inCart' Object field in localStorage
+
+function cartQuantity(item, action) { // increments 'inCart' Object field in localStorage
     // get current 'CartQuantity' (False if none); parse Int
     let itemQuantity = parseInt(localStorage.getItem('cartQuantity'));
     if(itemQuantity) { // if there True
@@ -52,29 +40,25 @@ function cartQuantityDown(item){
     let itemQuantity = parseInt(localStorage.getItem('cartQuantity'));
     if(itemQuantity) { // if there True
         // then increment; Also, update Cart <i>con
-        localStorage.setItem('cartQuantity', itemQuantity + 1);
+        localStorage.setItem('cartQuantity', itemQuantity - 1);
         // update Navbar Cart <icon> itemCount 
-        document.querySelector('.nav-cart-count').textContent = itemQuantity + 1;
-    } else { // False (NaN), then create 
-        localStorage.setItem('cartQuantity', 1);
-        // update Navbar Cart <icon> itemCount (initialize)
-        document.querySelector('.nav-cart-count').textContent = 1;
-    }
+        document.querySelector('.nav-cart-count').textContent = itemQuantity - 1;
+    } 
     decreaseItem(item)
 }
 function setItems(item){ // updates LocalStorage var 'itemsInCart'
     let cartItems = localStorage.getItem('itemsInCart'); // first, check what's there
     cartItems = JSON.parse(cartItems); // parse to js object
-    if (cartItems != null) { // if item exists in cart
-        if (cartItems[item.tag] == undefined) { // if NEW (first) 'cartItem' in the existing cart
+    if (cartItems != null) { // if cart exists
+        if (cartItems[item.tag] == undefined) { // if NEW item
             cartItems = {  
-                ...cartItems, // append to existing list of item Objects (js rest operator)
-                [item.tag]: item // add this new item Object
+                ...cartItems, // append to existing items (rest operator)
+                [item.tag]: item // ... this new item
             }
         }
-        cartItems[item.tag].inCart += 1; // item's "inCart" value increment
-    } else { // if this type of item doesn't exist in cart
-        item.inCart = 1; // item's count initialized
+        cartItems[item.tag].inCart += 1; // increase item's count
+    } else { // create the cart
+        item.inCart = 1; // with this item in it
         cartItems = { 
             [item.tag]: item // 
         }
@@ -97,26 +81,53 @@ function decreaseItem(item){
     localStorage.setItem("itemsInCart", JSON.stringify(cartItems));
 }
 // "Remove From Cart" Button
-function removeItem(item){
-    let cartItems = localStorage.getItem('itemsInCart'); // first, check what's there
-    cartItems = JSON.parse(cartItems); // parse to js object
-    if (cartItems != null) { // if item exists in cart
-        cartItems[item.tag].inCart = 0; // item's "inCart" value increment
-    } 
-    localStorage.setItem("itemsInCart", JSON.stringify(cartItems));
-}
-function totalCost(item) {
+// function removeItem(item){
+//     let cartItems = localStorage.getItem('itemsInCart'); // first, check what's there
+//     cartItems = JSON.parse(cartItems); // parse to js object
+//     if (cartItems != null) { // if item exists in cart
+//         cartItems[item.tag].inCart = 0; // item's "inCart" value increment
+//     } 
+//     localStorage.setItem("itemsInCart", JSON.stringify(cartItems));
+// }
+
+// Computational Functions
+function totalCost(item,action) {
     // console.log("The item price is", item.price);
     let cartCost = localStorage.getItem('totalCost');
-    console.log("My cartCost is", cartCost);
-    console.log(typeof cartCost);
     
-    if (cartCost != null) {
-        cartCost = parseFloat(cartCost);
-        localStorage.setItem('totalCost', cartCost + item.price)
-    } else {
-        localStorage.setItem("totalCost", item.price);
+    if (action == 'increment') {
+        if (cartCost != null) {
+            
+            cartCost = parseFloat(cartCost);
+            localStorage.setItem('totalCost', cartCost + item.price)
+            // depending on 'action' (upArrow,downArrow,removeButton)
+            
+        } else {
+            localStorage.setItem("totalCost", item.price);
+        }
+    
+    } else if (action == 'decrement') {
+        //
+        localStorage.setItem('totalCost', cartCost - item.price)
     }
+    
+    
+    
+}
+/// Event Listeners & Display function below
+let carts = document.querySelectorAll('.add-cart') 
+for (let i = 0; i < carts.length; i++) {
+    carts[i].addEventListener('click', () => {
+        cartQuantity(items[i]);
+        totalCost(items[i],'increment');
+        displayCart()
+    });
+} 
+function displayNavBar(){ // CALLED at END of script
+    let itemQuantity = parseInt(localStorage.getItem('cartQuantity'));
+    if (itemQuantity) { // if Int exists, display it  
+        document.querySelector('.nav-cart-count').textContent = itemQuantity;
+    } 
 }
 function displayCart() {
     let cartItems = localStorage.getItem("itemsInCart");
@@ -190,7 +201,7 @@ function displayCart() {
     arrowsUp[i].addEventListener('click', () => {
         console.log("arrow")
         cartQuantity(items[i]);
-        totalCost(items[i]);
+        totalCost(items[i],'increment');
         displayCart()
     });
     }
@@ -199,7 +210,7 @@ function displayCart() {
     arrowsDown[i].addEventListener('click', () => {
         console.log("arrow")
         cartQuantityDown(items[i]);
-        totalCost(items[i]);
+        totalCost(items[i],'decrement');
         displayCart()
     });
     }
@@ -216,100 +227,6 @@ function displayCart() {
 
 }
 
-// // once document is loaded, execute functions
-// if (document.readyState == 'loading') {
-//     document.addEventListener('DOMContentLoaded', ready)
-// } else {
-//     ready()
-// }
-
-// function ready(){
-//     // ADD EVENT LISTENERS...
-//     // To "Remove" buttons, store in var, assign "Click" listener...
-//     var removeCartItemButtons = document.getElementsByClassName('cart-item__remove-button')
-//     for (var i = 0; i < removeCartItemButtons.length; i++) {
-//         // ...add to to this tag 
-//         var button = removeCartItemButtons[i]
-//         button.addEventListener('click', removeCartItem)
-//     }
-//     // To "quantity" <input> tags, assign "change" listener...
-//     var quantityInputs = document.getElementsByClassName('cart-item__quantity')
-//     for (var i = 0; i < quantityInputs.length; i++) {
-//         // ...add to to this tag 
-//         var input = quantityInputs[i]
-//         input.addEventListener('change', quantityChanged)
-//     }
-//     // To "quantity UP" <i> tags, assign "change" listener...
-//     var quantityUp = document.getElementsByClassName("qty-increment");  
-//     for (var i = 0; i < quantityUp.length; i++) {
-//         // ...add to to this tag 
-//         var qtyUp = quantityUp[i]
-//         qtyUp.addEventListener('click', quantityIncrement)
-//     }
-//     // To "quantity DOWN" <i> tags, assign "change" listener...
-//     var quantityDown = document.getElementsByClassName("qty-decrement");  
-//     for (var i = 0; i < quantityDown.length; i++) {
-//         // ...add to to this tag 
-//         var qtyDown = quantityDown[i]
-//         qtyDown.addEventListener('click', quantityDecrement)
-//     }
-// }
-// // REDO
-// function updateCartTotal(){
-//     // var itemSubtotals = []
-//     // var itemSubtotal = 0;
-//     // var items_price = document.querySelectorAll(".cart")
-//     var cartItemContainer = document.getElementsByClassName('cart')[0]
-//     var cartItems = cartItemContainer.getElementsByClassName('cart-item')
-//     for (var i = 0; i < cartItems.length; i++){
-//         var cartItem = cartItems[i]
-//         var priceElement = cartItem.getElementsByClassName('cart-item__price')[0].innerText
-//         console.log(priceElement)
-//         var quantityElement = cartItem.getElementsByClassName('cart-item__quantity')[0]
-    
-//         var price = parseFloat(priceElement.innerText.replace('$', ''))
-//         var quantity = quantityElement.value
-//         itemSubtotal = price * quantity
-//         subtotal += price * quantity
-//         cartItems[i].getElementsByClassName('cart-item__item-total').innerText = '$' + itemSubtotal
-//         // console.log(itemSubtotal)
-
-//     }
-//     document.getElementsByClassName('js_subtotal').innerText = subtotal
-    
-// }
-// function removeCartItem(event) {    
-//     var buttonClicked = event.target
-//     buttonClicked.parentElement.parentElement.parentElement.remove()
-//     updateCartTotal()    
-// }
-// function quantityChanged(event) {
-//     var input = event.target
-//     if (isNaN(input.value) || input.value <= 0) {
-//         input.value = 1
-//     }
-//     // updateCartTotal();
-// }
-
-// // function quantityIncrement(event) {
-// //     // get item's inCart value from itemsInCart
-// //     // increment to item.inCart
-// //     // update the subtotal, and cartTotal
-// //     let cartItems = localStorage.getItem('itemsInCart');
-    
-// //     console.log(cartItems);
-
-// //     // updateCartTotal();
-// // }
-// // function quantityDecrement(event) {
-
-// //     cartQuantity(event)
-// // }
-
-
-
-
-
 
 // $(".checkout").toggle();
 $("#checkout").click(function(){
@@ -320,7 +237,7 @@ $("#checkout").click(function(){
 
 
 // Run at Load
-onLoadCartQuantity();
+displayNavBar();
 displayCart();
 
 
