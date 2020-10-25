8a0637640caa1e14b4cb580d2b87b8e308177416
cryskim3 <skim552@my.bcit.ca>
2020-10-25 10:05:15 -0700

    updating branch

---
+++ scripts/MeetOurTeam.js
@@ -0,0 +1,48 @@
+$(".card-text111").on("click", function () {
+  $(".avail_time111").animate(
+    {
+      height: "toggle",
+    },
+    1000
+  );
+});
+$(".card-text222").on("click", function () {
+  $(".avail_time222").animate(
+    {
+      height: "toggle",
+    },
+    1000
+  );
+});
+$(".card-text333").on("click", function () {
+  $(".avail_time333").animate(
+    {
+      height: "toggle",
+    },
+    1000
+  );
+});
+$(".card-text444").on("click", function () {
+  $(".avail_time444").animate(
+    {
+      height: "toggle",
+    },
+    1000
+  );
+});
+$(".card-text555").on("click", function () {
+  $(".avail_time555").animate(
+    {
+      height: "toggle",
+    },
+    1000
+  );
+});
+$(".card-text666").on("click", function () {
+  $(".avail_time666").animate(
+    {
+      height: "toggle",
+    },
+    1000
+  );
+});
--- scripts/landing.js
+++ scripts/landing.js
@@ -1,44 +1,41 @@
 // ** Slick JS ** //
-$(document).ready(function(){
-    $('.responsive').slick({
-        dots: true,
-        infinite: true,
-        autoplay: true,
-        speed: 300,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-
-        ]
-      });
+$(document).ready(function () {
+  $(".responsive").slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 });
 
 // Hover effect on cards
-$(".hover").mouseleave(
-  function () {
-    $(this).removeClass("hover");
-  }
-);
+$(".hover").mouseleave(function () {
+  $(this).removeClass("hover");
+});
